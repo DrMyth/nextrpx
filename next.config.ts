30fa4dbca331a6eb43121092bcd9e24179e571d5
Varun Maramreddy <128990962+DrMyth@users.noreply.github.com>
2025-03-13 20:28:31 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,17 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   async rewrites() {
-    return [
-      {
-        source: "/projects/:username/landing",
-        destination: "/",
-      },
-      {
-        source: "/projects/:username/landing/:path*",
-        destination: "/:path*",
-      },
-    ];
-  },
+  return [
+    {
+      source: "/projects/:username/landing/_next/:path*",
+      destination: "/_next/:path*",
+    },
+    {
+      source: "/projects/:username/landing/:path*",
+      destination: "/:path*",
+    },
+    {
+      source: "/projects/:username/landing",
+      destination: "/",
+    },
+  ];
+}
 };
 
 export default nextConfig;
